07b0febf327f723abd6971d5c38ce5ab6c3d931b
jbisits <75812103+jbisits@users.noreply.github.com>
2023-01-08 12:05:12 +1100

    Split tests up

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,16 @@
 using OceanRasterConversions, Test, Rasters, GibbsSeaWater
 
-@testset "ocean conversions" begin
+include("test_oceanrasterconversions.jl")
 
-    include("test_oceanrasterconversions.jl")
+@testset "RasterStack conversions" begin
 
     ## `depth_to_pressure`
     @test isequal(converted_p, p)
     ## `Sₚ_to_Sₐ`
     @test isequal(converted_Sₚ, Sₐ_)
-    @test isequal(converted_Sₚ_series, Sₐ_)
     ## `θ_to_Θ`
     @test isequal(converted_θ, Θ)
-    @test isequal(converted_θ_series, Θ)
-
     ## `convert_ocean_vars`
-    ## `RasterStack`s tests
     # In situ density
     for (i, var) ∈ enumerate(test_vars_in_situ)
         @test isequal(rs_stack_res_in_situ[var], vars_in_situ[i])
@@ -24,8 +20,16 @@ using OceanRasterConversions, Test, Rasters, GibbsSeaWater
     for (i, var) ∈ enumerate(test_vars_pd)
         @test isequal(rs_stack_res_pd[var], vars_pd[i])
     end
+end
 
-    ## `RasterSeries`s tests
+@testset "RasterSeries conversions" begin
+
+    ## `Sₚ_to_Sₐ`
+    @test isequal(converted_Sₚ_series, Sₐ_)
+    ## `θ_to_Θ`
+    @test isequal(converted_θ_series, Θ)
+
+    ## `convert_ocean_vars`
     # In situ density
     for t ∈ eachindex(rs_series)
         for (i, var) ∈ enumerate(test_vars_in_situ)
@@ -38,5 +42,4 @@ using OceanRasterConversions, Test, Rasters, GibbsSeaWater
             @test isequal(rs_series_res_pd[Ti(t)][var], vars_pd[i][:, :, :, t])
         end
     end
-
 end
